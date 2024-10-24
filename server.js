2aca0ece379308b108f3769e294102531937d613
tiagocosta225 <tiagocosta225@gmail.com>
2024-10-24 11:42:35 -0300

    Alterando a porta no server.js

--- server.js
+++ server.js
@@ -2,30 +2,13 @@ import { fastify } from 'fastify'
 import { DatabaseMemory } from './database-memory.js'
 import { DatabasePostgres } from './database-postgres.js'
 
-// Importa o Fastify
-const fastify = require('fastify')({ logger: true });
 
 const server = fastify()
-
-// Define a porta, utilizando variável de ambiente se disponível
-const port = process.env.PORT || 4000;
 
 //const database = new DatabaseMemory()
 
 const database = new DatabasePostgres()
 
-// Inicia o servidor, escutando em '0.0.0.0' para tornar acessível publicamente
-const start = async () => {
-  try {
-    await fastify.listen({ port: port, host: '0.0.0.0' });
-    fastify.log.info(`Server is running on port ${port}`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-};
-
-start();
 
 server.post('/videos', async (request, reply) => {
     const {title, description, duration} = request.body
